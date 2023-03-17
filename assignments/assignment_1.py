fa51e25f2294075ff52a62f8f5f99d2e20c9aeed
Aaron Will Djaba <papiliocurtis@gmail.com>
2023-03-17 23:40:44 +0000

    Taught python variables :fire:

---
+++ assignments/assignment_1.py
@@ -0,0 +1,24 @@
+""" 
+Write a python script, declare 4 variables. 
+1. two string variables that will take store name and store contact
+2. two lists that will take products and their prices respectfully
+print them out
+"""
+
+# string variables
+storeName = "Mega Store"
+storeContact = "0567316733"
+
+# list variables
+products = ["belt", "amane", "gari", "dawadawa", "beans"]
+price = [78.00, 10.00, 35.00, 5.00, 9.00]
+
+
+print(f"Store Name: {storeName}")
+print(f"Store Contact: {storeContact}")
+print("========Products=========")
+print(products[0],price[0])
+print(products[1],price[1])
+print(products[2],price[2])
+print(products[3],price[3])
+print(products[4],price[4])
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print("Hello World!")
 
+# Declaring variables
+userName = "Aaron"
+userAge = 76
+userGender = "Male"
+items = ["banana", "tooth brush", "chicken", "Rolls Royce", "G-wagon", "Aboboyaa", "Benz"]
+
+print(userName)
+print(userAge)
+print(userGender)
+print(items[6])
+print(len(items))
+print(userGender[3])
